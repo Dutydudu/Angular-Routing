3ae0cb852b08a756cb4fed111ae6158b7cc9bfa4
Dutydudu <79476368+Dutydudu@users.noreply.github.com>
2022-10-27 20:40:48 -0300

    2 files modified

--- src/app/components/home-component/home-component.component.ts
+++ src/app/components/home-component/home-component.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
-  styleUrls: ['./home-component.component.css']
+  styleUrls: ['./home-component.component.css'],
 })
 export class HomeComponentComponent implements OnInit {
+  name: string = 'Luis Eduardo';
+  lastname: string = 'Duarte';
+  ra: string = '0050832021022';
+  date = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
